3b1ecfab8dc9107e45dbf3677ae12898523356bf
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-09-17 10:59:31 +0200

    verticalmoviescards tags

--- src/components/VerticalMovieCard.tsx
+++ src/components/VerticalMovieCard.tsx
@@ -2,11 +2,17 @@ import { IMovie } from "../types/types"
 import '../style/VerticalMovieCard.css'
 
 function VerticalMovieCard({movie, moviesBg} : {movie : IMovie, moviesBg : string}){
+
+    const movieGenres = movie.Genre.split(', ')
+
     return(
         <article key={movie.imdbID} className="movieCard">
             <div className="movieInfoContainer">
                 <h3>{movie.Title}</h3>
-                <p>{movie.Plot}</p>
+                <div style={{marginTop:'4px'}} className="genreTagsContainer">
+                    {movieGenres.map(genre => { return(<div className="genreTag">{genre}</div>) })}
+                </div>
+                <p style={{marginTop:'6px'}}>{movie.Plot}</p>
             </div>
             <img className="posterImg" src={movie.Poster}/>
             <img className="movieBgImg" src={moviesBg}/>
