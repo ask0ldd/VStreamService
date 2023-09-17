507455091b10e79b36faa60396e423307cce6a1b
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-09-17 06:37:36 +0200

    arrow scrolling moviescontainer ok

--- src/components/MoviesSlideshow.tsx
+++ src/components/MoviesSlideshow.tsx
@@ -12,11 +12,29 @@ function MoviesSlideshow({moviesList}: {moviesList : IMovie[]}) {
         'tt8111088' : 'mandalorian.jpg',
     }
 
+    function scrollRight(e : React.MouseEvent<HTMLElement>){
+        e.preventDefault()
+        const arrow = e.currentTarget as HTMLImageElement
+        if(arrow.parentElement) arrow.parentElement.scrollBy({
+            left: 462,
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+
     return (
         
         <section>
             <h2>Currently Trending</h2>
             <div className="moviesContainer">
+                <img onClick={scrollRight} className="arrow" src="arrow-right.svg"/>
+                {moviesList.map(movie => (
+                    <article key={movie.imdbID} className="movieCard">
+                        <h3>{movie.Title}</h3>
+                        <img className="posterImg" src={movie.Poster}/>
+                        <img className="movieBgImg" src={moviesBg[movie.imdbID]}/>
+                    </article>
+                ))}
                 {moviesList.map(movie => (
                     <article key={movie.imdbID} className="movieCard">
                         <h3>{movie.Title}</h3>
