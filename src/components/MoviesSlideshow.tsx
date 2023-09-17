de533fc1b66e9caa432106cf78d2e66eeaaada06
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-09-17 05:51:57 +0200

    movie cards bgs

--- src/components/MoviesSlideshow.tsx
+++ src/components/MoviesSlideshow.tsx
@@ -2,6 +2,16 @@ import { IMovie } from "../types/types"
 import '../style/MoviesSlideshow.css'
 
 function MoviesSlideshow({moviesList}: {moviesList : IMovie[]}) {
+
+    const moviesBg : IMoviesBg = {
+        'tt3230854' : 'theexpanse2.jpg', 
+        'tt0804484' : 'foundation.png', 
+        'tt14688458' : 'silo.jpg', 
+        'tt11280740' : 'severance.jpg', 
+        'tt3581920' : 'thelastofus.jpg', 
+        'tt8111088' : 'mandalorian.jpg',
+    }
+
     return (
         
         <section>
@@ -11,7 +21,7 @@ function MoviesSlideshow({moviesList}: {moviesList : IMovie[]}) {
                     <article key={movie.imdbID} className="movieCard">
                         <h3>{movie.Title}</h3>
                         <img className="posterImg" src={movie.Poster}/>
-                        <img className="movieBgImg" src='mandalorian.jpg'/>
+                        <img className="movieBgImg" src={moviesBg[movie.imdbID]}/>
                     </article>
                 ))}
             </div>
@@ -20,3 +30,7 @@ function MoviesSlideshow({moviesList}: {moviesList : IMovie[]}) {
 }
 
 export default MoviesSlideshow
+
+interface IMoviesBg {
+    [key: string]: string;
+  }
