59081e2919d2dad62a2fb9d5a464da6fac5e530f
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-09-18 08:05:33 +0200

    fonts import

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ function Header (){
     return(
         <header>
             <figure>
-                WatchFusion
+                <img src="mainlogo.png"/>
             </figure>
             <nav>
                 <ul>
