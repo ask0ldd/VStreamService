720e706e3e51ef66f13283b44969d65e7d0d8eb3
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-09-18 07:08:54 +0200

    header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+function Header (){
+    return(
+        <header>
+            <figure>
+                WatchFusion
+            </figure>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Watchlist</li>
+                    <li>Categories</li>
+                    <li>Settings</li>
+                    <li>Logout</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
