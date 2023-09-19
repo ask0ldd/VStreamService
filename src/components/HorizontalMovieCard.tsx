070609bf19e87a9dac694fafbb1f1ca7f8a548dd
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-09-19 13:20:38 +0200

    horiz movie card play button

--- src/components/HorizontalMovieCard.tsx
+++ src/components/HorizontalMovieCard.tsx
@@ -11,6 +11,11 @@ function HorizontalMovieCard ({movie, moviesBg} : {movie : IMovie, moviesBg : st
                     <div className="moviePositionBar"></div>
                 </div>
             </div>
+            <div className="playAddMoreContainer">
+                <div className="playContainer">
+                    <a href="#" style={{height:'48px', borderRadius:'300px'}}><img style={{height:'48px', boxShadow:'0px 4px 8px #00000066', borderRadius:'300px'}} src="playbutton.png"/></a>
+                </div>
+            </div>
             <div className="movieInfosContainer">
                 <h3>{movie.Title}</h3>
                 <div>{movie.Released} {movie.Runtime} {movie.Rated}</div>
