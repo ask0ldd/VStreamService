b441f99969bb8489553ba703334f733ce8ccbb1c
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-09-23 20:33:51 +0200

    movie page movie texts

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -1,13 +1,29 @@
 import Footer from "../components/Footer"
 import Header from "../components/Header"
+import './Movie.css'
 
 function Movie(){
+
+
+
     return(
         <>
             <Header/>
             <main className="moviePage">
-                <section className="moviePicContainer">
-                    <img className="mainMoviePic" src="/theboys.jpg"/>
+                <section className="moviePContainer">
+                    <h2>THE BOYS</h2>
+                    <span className="plot">
+                        The Boys is set in a universe where super-powered individuals are recognized as heroes by the general public and work for a powerful 
+                        corporation known as Vought International, which markets and monetizes them. Outside of their heroic personas, most are arrogant, 
+                        selfish, and corrupt.
+                    </span>
+                    <ul className="tagsList">
+                        <li>7.5</li>
+                        <li>2019</li>
+                        <li>PG-13</li>
+                        <li>HDR</li>
+                        <li>UHD</li>
+                    </ul>
                 </section>
             </main>
             <Footer/>
@@ -16,3 +32,5 @@ function Movie(){
 }
 
 export default Movie
+
+// <img className="mainMoviePic" src="/theboys.jpg"/>
