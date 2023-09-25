6405ddb134b25acdc75155395160e89684844659
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-09-25 17:52:29 +0200

    episodes cards lazy loading

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from "react"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import './Movie.css'
+import { seteuid } from "process"
 
 function Movie(){
 
     const [activeSeason, setActiveSeason] = useState<number>(1)
+    const [episodesShown, setEpisodesShown] = useState<number>(2)
 
     function videoStart(){
         const video = document.querySelector('video') as HTMLVideoElement
@@ -24,6 +27,34 @@ function Movie(){
             videoStart()
         } , 3000)
     },[])
+
+    // sets Intersection Observer displaying more episodes when at the bottom of the page
+    useEffect(() => {
+        const episodeCard = document.getElementById("episodeCard"+episodesShown)
+
+        const options = {
+            root: null,
+            rootMargin: "0px",
+            threshold: 1.0,
+        }
+
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    console.log('ohohohho')
+                    setEpisodesShown(4)
+                    observer.disconnect()
+                }
+            })
+        }, options)
+
+        if(episodeCard != null) observer.observe(episodeCard)
+
+        // cleanup
+        /*return () => {
+
+        }*/
+    }, [])
 
     return(
         <>
@@ -75,9 +106,11 @@ function Movie(){
                         <li onClick={() => setActiveSeason(1)} className={activeSeason === 1 ? "seasonTag active" : "seasonTag inactive"}>Season 1</li>
                         <li onClick={() => setActiveSeason(2)} className={activeSeason === 2 ? "seasonTag active" : "seasonTag inactive"}>Season 2</li>
                         <li onClick={() => setActiveSeason(3)} className={activeSeason === 3 ? "seasonTag active" : "seasonTag inactive"}>Season 3</li>
+                        <li className="seasonTag inactive" style={{marginLeft:'auto'}}>24 Episodes</li>
                     </ul>
 
-                    <article>
+                    {episodesShown > 0 && 
+                    <article id="episodeCard1">
                         <figure>
                             <img src="../theboys-1x1.png"/>
                             <div className="timePositionContainer">
@@ -112,8 +145,10 @@ function Movie(){
                             </div>
                         </div>
                     </article>
+                    }
 
-                    <article>
+                    {episodesShown > 1 &&
+                    <article id="episodeCard2">
                         <figure>
                             <img style={{filter:'saturate(0.9) contrast(0.9) brightness(1.1)'}} src="../theboys-1x2.png"/>
                             <div className="timePositionContainer">
@@ -148,8 +183,10 @@ function Movie(){
                             </div>
                         </div>
                     </article>
+                    }
 
-                    <article>
+                    {episodesShown > 2 &&
+                    <article id="episodeCard3">
                         <figure>
                             <img style={{filter:'saturate(0.8) contrast(0.82) brightness(1.2)'}} src="../theboys-1x3.png"/>
                             <div className="timePositionContainer">
@@ -184,8 +221,10 @@ function Movie(){
                             </div>
                         </div>
                     </article>
+                    }
 
-                    <article>
+                    {episodesShown > 3 &&
+                    <article id="episodeCard4">
                         <figure>
                             <img src="../theboys-1x1.png"/>
                         </figure>
@@ -217,6 +256,80 @@ function Movie(){
                             </div>
                         </div>
                     </article>
+                    }
+
+                    {episodesShown > 4 &&
+                    <article id="episodeCard5">
+                        <figure>
+                            <img style={{filter:'saturate(0.8) contrast(0.82) brightness(1.2)'}} src="../theboys-1x3.png"/>
+                            <div className="timePositionContainer">
+                                <div className="timePositionBlueBar"></div>
+                            </div>
+                        </figure>
+                        <div className="episodeInfosContainer">
+                            <h3>S1.E3 ∙ Get Some</h3>
+                            <ul>
+                                <li>
+                                    8.9
+                                    <svg style={{marginTop:'-1.25px'}} xmlns="http://www.w3.org/2000/svg" fill="#1A98FF" height="1em" viewBox="0 0 576 512">
+                                        <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/>
+                                    </svg>
+                                </li>
+                                <li>2019</li>
+                                <li>PG-13</li>
+                                <li>45 min</li>
+                            </ul>
+                            <p>
+                                You and a guest are invited to the premiere of DAWN OF THE SEVEN this Tuesday at 7PM in Vought Tower’s 
+                                Lamplighter Memorial Theater! Screening will be followed by a Q&A with director Adam Bourke and an after-party 
+                                with Supe DJ Malchemical on the turntables! Join us for the greatest true origin story ever told about America’s 
+                                number one Superhero team and see how the legend began!
+                            </p>
+                            <div className="episodesButtonsContainer">
+                                <img src="/buttons/likebutton.png"/>
+                                <img src="/buttons/dislikebutton.png"/>
+                                <img src="/buttons/addbutton.png"/>
+                                <img src="/buttons/morebutton.png"/>
+                                <img src="/buttons/play.png"/>
+                            </div>
+                        </div>
+                    </article>
+                    }
+
+                    {episodesShown > 5 &&
+                    <article id="episodeCard6">
+                        <figure>
+                            <img src="../theboys-1x1.png"/>
+                        </figure>
+                        <div className="episodeInfosContainer">
+                            <h3>S1.E4 ∙ The Female of the Species</h3>
+                            <ul>
+                                <li>
+                                    8.9
+                                    <svg style={{marginTop:'-1.25px'}} xmlns="http://www.w3.org/2000/svg" fill="#1A98FF" height="1em" viewBox="0 0 576 512">
+                                        <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/>
+                                    </svg>
+                                </li>
+                                <li>2019</li>
+                                <li>PG-13</li>
+                                <li>45 min</li>
+                            </ul>
+                            <p>
+                                You and a guest are invited to the premiere of DAWN OF THE SEVEN this Tuesday at 7PM in Vought Tower’s 
+                                Lamplighter Memorial Theater! Screening will be followed by a Q&A with director Adam Bourke and an after-party 
+                                with Supe DJ Malchemical on the turntables! Join us for the greatest true origin story ever told about America’s 
+                                number one Superhero team and see how the legend began!
+                            </p>
+                            <div className="episodesButtonsContainer">
+                                <img src="/buttons/likebutton.png"/>
+                                <img src="/buttons/dislikebutton.png"/>
+                                <img src="/buttons/addbutton.png"/>
+                                <img src="/buttons/morebutton.png"/>
+                                <img src="/buttons/play.png"/>
+                            </div>
+                        </div>
+                    </article>
+                    }
 
                 </section>
             </main>
