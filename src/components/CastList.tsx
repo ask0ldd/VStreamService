88d34bc4726956f53c9188a9eb1d5078a19a8fc8
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-09-28 16:37:48 +0200

    movie page : cast list full

--- src/components/CastList.tsx
+++ src/components/CastList.tsx
@@ -7,7 +7,7 @@ function CastList(){
             <div className='castContainer'>
                 
                 <article className='castArticle'>
-                    <picture><img src="../cast/starr.jpg"/></picture>
+                    <picture><img src="../cast/urban.jpg"/></picture>
                     <div className='castMemberInfos'>
                         <h4>Karl Urban</h4>
                         <p className='characterName'><span>as</span> Billy Butcher</p>
@@ -17,66 +17,91 @@ function CastList(){
                 <article className='castArticle'>
                     <picture><img src="../cast/starr.jpg"/></picture>
                     <div className='castMemberInfos'>
-                        <h4>Karl Urban</h4>
-                        <p className='characterName'><span>as</span> Billy Butcher</p>
+                        <h4>Anthony Starr</h4>
+                        <p className='characterName'><span>as</span> Homelander</p>
                         <p className='appearances'>32 episodes • 2019–2022</p>
                     </div>
                 </article>
                 <article className='castArticle'>
-                    <picture><img src="../cast/starr.jpg"/></picture>
+                    <picture><img src="../cast/quaid.jpg"/></picture>
                     <div className='castMemberInfos'>
-                        <h4>Karl Urban</h4>
-                        <p className='characterName'><span>as</span> Billy Butcher</p>
-                        <p className='appearances'>32 episodes • 2019–2022</p>
-                    </div>
-                </article>
-
-                <article className='castArticle'>
-                    <picture><img src="../cast/starr.jpg"/></picture>
-                    <div className='castMemberInfos'>
-                        <h4>Karl Urban</h4>
-                        <p className='characterName'><span>as</span> Billy Butcher</p>
-                        <p className='appearances'>32 episodes • 2019–2022</p>
-                    </div>
-                </article>
-                <article className='castArticle'>
-                    <picture><img src="../cast/starr.jpg"/></picture>
-                    <div className='castMemberInfos'>
-                        <h4>Karl Urban</h4>
-                        <p className='characterName'><span>as</span> Billy Butcher</p>
-                        <p className='appearances'>32 episodes • 2019–2022</p>
-                    </div>
-                </article>
-                <article className='castArticle'>
-                    <picture><img src="../cast/starr.jpg"/></picture>
-                    <div className='castMemberInfos'>
-                        <h4>Karl Urban</h4>
-                        <p className='characterName'><span>as</span> Billy Butcher</p>
+                        <h4>Jack Quaid</h4>
+                        <p className='characterName'><span>as</span> Hughie Campbell</p>
                         <p className='appearances'>32 episodes • 2019–2022</p>
                     </div>
                 </article>
 
                 <article className='castArticle'>
-                    <picture><img src="../cast/starr.jpg"/></picture>
+                    <picture><img src="../cast/moriarty.jpg"/></picture>
                     <div className='castMemberInfos'>
-                        <h4>Karl Urban</h4>
-                        <p className='characterName'><span>as</span> Billy Butcher</p>
+                        <h4>Erin Moriarty</h4>
+                        <p className='characterName'><span>as</span> Annie January</p>
                         <p className='appearances'>32 episodes • 2019–2022</p>
                     </div>
                 </article>
                 <article className='castArticle'>
-                    <picture><img src="../cast/starr.jpg"/></picture>
+                    <picture><img src="../cast/usher.jpg"/></picture>
                     <div className='castMemberInfos'>
-                        <h4>Karl Urban</h4>
-                        <p className='characterName'><span>as</span> Billy Butcher</p>
+                        <h4>Jessie T. Usher</h4>
+                        <p className='characterName'><span>as</span> A-Train</p>
                         <p className='appearances'>32 episodes • 2019–2022</p>
                     </div>
                 </article>
                 <article className='castArticle'>
-                    <picture><img src="../cast/starr.jpg"/></picture>
+                    <picture><img src="../cast/alonzo.jpg"/></picture>
                     <div className='castMemberInfos'>
-                        <h4>Karl Urban</h4>
-                        <p className='characterName'><span>as</span> Billy Butcher</p>
+                        <h4>Laz Alonso</h4>
+                        <p className='characterName'><span>as</span> Mother's Milk</p>
+                        <p className='appearances'>32 episodes • 2019–2022</p>
+                    </div>
+                </article>
+
+                <article className='castArticle'>
+                    <picture><img src="../cast/crawford.jpg"/></picture>
+                    <div className='castMemberInfos'>
+                        <h4>Chace Crawford</h4>
+                        <p className='characterName'><span>as</span> The Deep</p>
+                        <p className='appearances'>32 episodes • 2019–2022</p>
+                    </div>
+                </article>
+                <article className='castArticle'>
+                    <picture><img src="../cast/capone.jpg"/></picture>
+                    <div className='castMemberInfos'>
+                        <h4>Tomer Capone</h4>
+                        <p className='characterName'><span>as</span> Frenchie</p>
+                        <p className='appearances'>32 episodes • 2019–2022</p>
+                    </div>
+                </article>
+                <article className='castArticle'>
+                    <picture><img src="../cast/fukuhara.jpg"/></picture>
+                    <div className='castMemberInfos'>
+                        <h4>Karen Fukuhara</h4>
+                        <p className='characterName'><span>as</span> Kimiko Miyashiro</p>
+                        <p className='appearances'>32 episodes • 2019–2022</p>
+                    </div>
+                </article>
+
+                <article className='castArticle'>
+                    <picture><img src="../cast/mitchell.jpg"/></picture>
+                    <div className='castMemberInfos'>
+                        <h4>Nathan Mitchell</h4>
+                        <p className='characterName'><span>as</span> Black Noir</p>
+                        <p className='appearances'>32 episodes • 2019–2022</p>
+                    </div>
+                </article>
+                <article className='castArticle'>
+                    <picture><img src="../cast/minifie.jpg"/></picture>
+                    <div className='castMemberInfos'>
+                        <h4>Colby Minifie</h4>
+                        <p className='characterName'><span>as</span> Ashley Barrett</p>
+                        <p className='appearances'>32 episodes • 2019–2022</p>
+                    </div>
+                </article>
+                <article className='castArticle'>
+                    <picture><img src="../cast/mcelligott.jpg"/></picture>
+                    <div className='castMemberInfos'>
+                        <h4>Dominique McElligott</h4>
+                        <p className='characterName'><span>as</span> Maggie Shaw</p>
                         <p className='appearances'>32 episodes • 2019–2022</p>
                     </div>
                 </article>
