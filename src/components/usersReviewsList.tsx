1ba9f845e2c3d8c79acb0e9c28470cadf7ea37f2
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-10-17 02:38:47 +0200

    reviewer col

--- src/components/usersReviewsList.tsx
+++ src/components/usersReviewsList.tsx
@@ -18,14 +18,18 @@ function UsersReviewsList(){
                         I've had big expectations about this show, because I thought that is going to be exactly that kind of show, in which the good guys (the superheroes) fight the bad guys (the villains) and of course, they win. I couldn't have been more wrong, especially because "The Boys" it's not about a desperate attempt of superheroes to save the world from evil, instead this show takes a much more realistic approach, about what is going to happen if superheroes had really existed.
                         </p>
                     </div>
-                    <img className='creatorPortrait' src="../cast/quaid.jpg"/>
+                    <div className='reviewCol'>
+                        <img className='creatorPortrait' src="../cast/quaid.jpg"/>
+                        <p className='by'>By</p>
+                        <p className='userName'>Tweakams</p>
+                    </div>
                 </div>
                 <div className='reviewSidePanel'>
                     aaa
                 </div>
             </article>
 
-            <article className='reviewFullRowContainer'>
+            <article className='reviewFullRowContainer' style={{marginTop:'3rem',}}>
                 <div className="usersReview">
                     <div className='reviewBody'>
                         <h3>It's exactly the opposite of your usual superhero show!</h3>
@@ -34,7 +38,11 @@ function UsersReviewsList(){
                         I've had big expectations about this show, because I thought that is going to be exactly that kind of show, in which the good guys (the superheroes) fight the bad guys (the villains) and of course, they win. I couldn't have been more wrong, especially because "The Boys" it's not about a desperate attempt of superheroes to save the world from evil, instead this show takes a much more realistic approach, about what is going to happen if superheroes had really existed.
                         </p>
                     </div>
-                    <img className='creatorPortrait' src="../cast/quaid.jpg"/>
+                    <div className='reviewCol'>
+                        <img className='creatorPortrait' src="../cast/quaid.jpg"/>
+                        <p className='by'>By</p>
+                        <p className='userName'>Tweakams</p>
+                    </div>
                 </div>
                 <div className='reviewSidePanel'>
                     aaa
