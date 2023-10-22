3f40463ba0ed506506ff309a119e5915fe8cf530
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-10-22 20:58:40 +0200

    slideshow : basic elements display test

--- src/__tests__/components/HorizontalMovieCardsSlideshow.test.tsx
+++ src/__tests__/components/HorizontalMovieCardsSlideshow.test.tsx
@@ -11,59 +11,21 @@ const MockedRouter = () => {
     return(
         <Provider store={store}>
             <BrowserRouter>
-                <HorizontalMovieCardsSlideshow slideshowTitle="Previously Watched" moviesList={moviesList2}/>
+                <HorizontalMovieCardsSlideshow slideshowTitle="Generic Title" moviesList={moviesList2}/>
             </BrowserRouter>
         </Provider>
     )
 }
 
-describe('App Page', async () => { 
-
-    // mocking the intersection observer obj
-    beforeAll(() => {
-        const IntersectionObserverMock = vi.fn(() => ({ 
-            disconnect: vi.fn(), 
-            observe: vi.fn(), 
-            takeRecords: vi.fn(), 
-            unobserve: vi.fn(), 
-        }))
-        vi.stubGlobal('IntersectionObserver', IntersectionObserverMock)
-    })
+describe('Horizontal Slideshow Component', async () => { 
 
     beforeEach(() => {
         render(<MockedRouter/>) 
     })
 
     test('The Banner Slideshow is displayed', async () => {
-        await waitFor(() => expect(screen.getByAltText('the dark knight batman')).toBeInTheDocument())
-        expect(screen.getByAltText('the dark knight bane')).toBeInTheDocument()
-        expect(screen.getByAltText('the dark knight catwoman')).toBeInTheDocument()
+        await waitFor(() => expect(screen.getByText('Generic Title')).toBeInTheDocument())
     })
-
-    test('The auto Banner Rotation is working', async () => {
-        await waitFor(() => expect(screen.getByAltText('the dark knight batman')).toBeInTheDocument())
-        const batmanBanner = screen.getByAltText('the dark knight batman')
-        const baneBanner = screen.getByAltText('the dark knight bane')
-        const catwomanBanner = screen.getByAltText('the dark knight catwoman')
-        expect(batmanBanner).not.toHaveStyle('transform: translateX(-1440px)')
-        expect(baneBanner).not.toHaveStyle('transform: translateX(-1440px)')
-        expect(catwomanBanner).not.toHaveStyle('transform: translateX(-1440px)')
-        await waitFor(() => {expect(batmanBanner).toHaveStyle('transform: translateX(-1440px)')}, { timeout: 7000 })
-    }, 10000) // expand delay before timeout
-
-    test('Clicking on the Banner trigger a rotation', async () => {
-        await waitFor(() => expect(screen.getByAltText('the dark knight batman')).toBeInTheDocument())
-        const batmanBanner = screen.getByAltText('the dark knight batman')
-        const baneBanner = screen.getByAltText('the dark knight bane')
-        const catwomanBanner = screen.getByAltText('the dark knight catwoman')
-        expect(batmanBanner).not.toHaveStyle('transform: translateX(-1440px)')
-        expect(baneBanner).not.toHaveStyle('transform: translateX(-1440px)')
-        expect(catwomanBanner).not.toHaveStyle('transform: translateX(-1440px)')
-        act(() => batmanBanner.click())
-        await waitFor(() => {expect(batmanBanner).toHaveStyle('transform: translateX(-1440px)')}, { timeout: 3000 })
-        expect(baneBanner).toHaveStyle('transform: translateX(-1440px)')
-        expect(catwomanBanner).toHaveStyle('transform: translateX(-1440px)')
-    }, 3000)
 
 })
 
